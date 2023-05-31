86545fa0e97f142192d18ce0a879efe51280a0d2
FrancoBertoldiMariglio <f.bertoldi@alumno.um.edu.ar>
2023-05-31 13:13:10 -0300

    hotfix

--- ejercicio13.py
+++ ejercicio13.py
@@ -1,3 +1,7 @@
+""" Escribir un programa que reciba un mensaje desde otro proceso usando fifo (pipes con nombre). 
+El proceso receptor deberá lanzar tantos hilos como líneas tenga el mensaje y deberá enviar cada línea a los hilos secundarios. 
+Cada hilo secundario deberá calcular la cantidad de caracteres de su línea y ¿COMPROBAR (asegurarse de que haya terminado) 
+la cuenta de la línea anterior?. """
 
 import multiprocessing as mp
 import threading as th
